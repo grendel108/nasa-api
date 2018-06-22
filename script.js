b6b3b2ef307f0187ddea61bc427d61e7571a2c42
Barry Lew <battosai108@gmail.com>
2018-06-21 21:21:03 -0700

    Initial version
    Returns APOD upon page load. Add additional functionality: button and date.

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// ADDITIONAL FUNCTIONS
+// 1. Add button to load image rather than page load.
+// 2. Add a textbox to ask for date and return that APOD.
+// 3. Connect Firebase to log each APOD that is loaded with the date as Key.
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+
+// Verify that JS file loaded.
+console.log("JS file has loaded.");
+
+// Do these have to be global? Why not declare within handleResponse()?
+// let titleAPODElem = document.getElementById("title");
+// let displayAPODElem = document.getElementById("apod");
+
+// titleAPODElem.textContent = "Test";
+
+
+
+let requestObject = new XMLHttpRequest();
+requestObject.addEventListener("load", handleResponse);
+requestObject.open("GET", "https://api.nasa.gov/planetary/apod?api_key=MZzdiyAR2POTssNKcTtkmmrZiQWV1pXrZXcAuCIc");
+requestObject.setRequestHeader("Accept", "application/json");  // Return JSON object to parse later.
+requestObject.send();
+
+function handleResponse () {
+    console.log(requestObject.responseText);
+
+    let titleAPODElem = document.getElementById("title");
+    let displayAPODElem = document.getElementById("apod");
+    let explainAPODElem = document.getElementById("explanation");
+    let apodResponseObject = JSON.parse(requestObject.responseText);
+
+    console.log(apodResponseObject.title);
+    console.log(apodResponseObject.url);
+
+    titleAPODElem.textContent = apodResponseObject.title;
+    displayAPODElem.src = apodResponseObject.url;
+    explainAPODElem.textContent = apodResponseObject.explanation;
+
+}
