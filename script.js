bca4244d2469594b7e42ee6cbc9e9d410cc5fd10
Barry Lew <battosai108@gmail.com>
2018-06-24 19:19:51 -0700

    Submit button for user-specified APOD
    Added textfield and submit button for user to get a past APOD.

--- script.js
+++ script.js
@@ -7,33 +7,63 @@
 // Verify that JS file loaded.
 console.log("JS file has loaded.");
 
-// Do these have to be global? Why not declare within handleResponse()?
-// let titleAPODElem = document.getElementById("title");
-// let displayAPODElem = document.getElementById("apod");
 
-// titleAPODElem.textContent = "Test";
+let titleAPODElem = document.getElementById("title");
+let dateAPODElem = document.getElementById("date");
+let displayAPODElem = document.getElementById("apod");
+let explainAPODElem = document.getElementById("explanation");
 
 
 
+// This can run once the user enters a date and clicks a button. Alternatively, the page loads today's APOD, and the user can specify a different day's APOD. Should the Date of APOD have a default value?
 let requestObject = new XMLHttpRequest();
 requestObject.addEventListener("load", handleResponse);
+// The URL should be a variable that is concatenated with the date that the member enters to call a past APOD.
 requestObject.open("GET", "https://api.nasa.gov/planetary/apod?api_key=MZzdiyAR2POTssNKcTtkmmrZiQWV1pXrZXcAuCIc");
 requestObject.setRequestHeader("Accept", "application/json");  // Return JSON object to parse later.
 requestObject.send();
 
-function handleResponse () {
-    console.log(requestObject.responseText);
 
-    let titleAPODElem = document.getElementById("title");
-    let displayAPODElem = document.getElementById("apod");
-    let explainAPODElem = document.getElementById("explanation");
+
+function handleResponse () {
+    console.log(requestObject);
+    console.log(requestObject.responseText);
     let apodResponseObject = JSON.parse(requestObject.responseText);
 
-    console.log(apodResponseObject.title);
-    console.log(apodResponseObject.url);
-
     titleAPODElem.textContent = apodResponseObject.title;
+    dateAPODElem.textContent = apodResponseObject.date;
     displayAPODElem.src = apodResponseObject.url;
     explainAPODElem.textContent = apodResponseObject.explanation;
 
+}
+
+let submitElem = document.getElementById("submitAPODDate");
+submitElem.addEventListener("click", handleClick);
+
+function handleClick(){
+
+    let apodDateElem = document.getElementById("apodDate");    
+    let apodDate = apodDateElem.value;
+    let api_key = "https://api.nasa.gov/planetary/apod?api_key=MZzdiyAR2POTssNKcTtkmmrZiQWV1pXrZXcAuCIc" + "&date=" + apodDate;
+    console.log(apodDate);
+
+    // Request the JSON object of APOD with user-specified date.
+    let requestObject = new XMLHttpRequest();
+    requestObject.addEventListener("load", handleResponse);
+    requestObject.open("GET", api_key);
+    console.log(api_key);
+    requestObject.setRequestHeader("Accept", "application/json");  // Return JSON object to parse later.
+    requestObject.send();
+
+    // This is called by the page load after the user clicked the submit button. Can this by DRY?
+    function handleResponse () {
+    apodResponseObject = JSON.parse(requestObject.responseText);
+    console.log(requestObject);
+    console.log(requestObject.responseText);
+
+    titleAPODElem.textContent = apodResponseObject.title;
+    dateAPODElem.textContent = apodResponseObject.date;
+    displayAPODElem.src = apodResponseObject.url;
+    explainAPODElem.textContent = apodResponseObject.explanation;
+    }
 }
