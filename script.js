1472fb048a7cc6efd0b9143fd94cc04c8e51fb7e
Barry Lew <battosai108@gmail.com>
2018-06-28 00:00:39 -0700

    Callback functions
    Add callback function to handle xhr object.

--- script.js
+++ script.js
@@ -8,50 +8,60 @@
 console.log("JS file has loaded.");
 
 
+// Get HTML elements as JS variables to display APOD.
 let titleAPODElem = document.getElementById("title");
 let dateAPODElem = document.getElementById("date");
 let displayAPODElem = document.getElementById("apod");
 let explainAPODElem = document.getElementById("explanation");
 let submitElem = document.getElementById("submitAPODDate");
-submitElem.addEventListener("click", handleClick);
+
+// API key. Add date parameter to get past APOD. Concatenate this with date parameter if user submits a date.
+let url = "https://api.nasa.gov/planetary/apod?api_key=MZzdiyAR2POTssNKcTtkmmrZiQWV1pXrZXcAuCIc";
+
+// Request APOD as JSON and then display the APOD.
+getAPOD(url, displayAPOD);
+getAPOD(url, myFunction);
 
 
-
-let requestObject = new XMLHttpRequest();
-requestObject.addEventListener("load", handleResponse);
-requestObject.open("GET", "https://api.nasa.gov/planetary/apod?api_key=MZzdiyAR2POTssNKcTtkmmrZiQWV1pXrZXcAuCIc");  // The URL should be a variable that is concatenated with the date that the member enters to call a past APOD.
-requestObject.setRequestHeader("Accept", "application/json");  // Return JSON object to parse later.
-requestObject.send();
-
-
-
-function handleClick(){
-
-    let apodDateElem = document.getElementById("apodDate");    
-    let apodDate = apodDateElem.value;
-    let api_key = "https://api.nasa.gov/planetary/apod?api_key=MZzdiyAR2POTssNKcTtkmmrZiQWV1pXrZXcAuCIc" + "&date=" + apodDate;
-    console.log(apodDate);
-
-    // Request the JSON object of APOD with user-specified date.
+//////////////////////  FUNCTION DEFINITIONS  ///////////////////////
+function getAPOD(url, callback) {
     let requestObject = new XMLHttpRequest();
-    requestObject.addEventListener("load", handleResponse);
-    requestObject.open("GET", api_key);
-    console.log(api_key);  // JSON object with prior date is being returned, but the object is not being passed to handleResponse().
-    requestObject.setRequestHeader("Accept", "application/json");  // Return JSON object to parse later.
-    requestObject.send();
+    requestObject.addEventListener("load", function () {
+        // Callback function--displayAPOD()--is now invoked within getAPOD(), which is the containing function.
+        callback(requestObject.response);
+        console.log("handleResponse() example");
+        console.log(requestObject.response);
+    })
 
+    // // This example code is from XMLHttpRequest.response. Seems to work the same as the event listener above.
+    // requestObject.onreadystatechange = function() {
+    //     if(requestObject.readyState === 4) {
+    //         callback(requestObject.response);
+    //         console.log("XMLHttpRequest.response example");
+    //         console.log(requestObject.response);
+    //     }
+    // }
+
+    requestObject.open("GET", url);
+    requestObject.setRequestHeader("Accept", "application/json");
+    requestObject.send();
+    
 }
 
 
-
-function handleResponse () {
-    console.log(requestObject);
-    console.log(requestObject.responseText);
-    let apodResponseObject = JSON.parse(requestObject.responseText);
-
+// displayAPOD() was passed the response by getAPOD().
+function displayAPOD(response) {
+    let apodResponseObject = JSON.parse(response);
     titleAPODElem.textContent = apodResponseObject.title;
     dateAPODElem.textContent = apodResponseObject.date;
     displayAPODElem.src = apodResponseObject.url;
     explainAPODElem.textContent = apodResponseObject.explanation;
 
 }
+
+// Pass a different callback function to getAPOD().
+function myFunction(x) {
+    console.log("Testing another callback function.");
+    console.log(JSON.parse(x).copyright);
+}
+/////////////////////////////////////////////////////////////////////
